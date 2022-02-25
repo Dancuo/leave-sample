b10d27c10fcc40f0fa814e31143231a658f39d4e
Jason Wang <dancuowang@163.com>
2022-02-26 01:55:00 +0800

    Add ignore file Upgrade dependency version Fix compile error

--- leave-service/src/main/java/ddd/leave/interfaces/facade/AuthApi.java
+++ leave-service/src/main/java/ddd/leave/interfaces/facade/AuthApi.java
@@ -1,17 +1,16 @@
 package ddd.leave.interfaces.facade;
 
 import ddd.leave.application.service.LoginApplicationService;
-import ddd.leave.domain.person.entity.Person;
 import ddd.leave.infrastructure.common.api.Response;
-import ddd.leave.interfaces.assembler.LeaveAssembler;
 import ddd.leave.interfaces.assembler.PersonAssembler;
-import ddd.leave.interfaces.dto.LeaveDTO;
 import ddd.leave.interfaces.dto.PersonDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,8 +23,8 @@ public class AuthApi {
     @PostMapping("/login")
     public Response login(PersonDTO personDTO){
         try {
-			return loginApplicationService.login(PersonAssembler.toDO(personDTO));
-		} catch (ParseException e) {
+            return loginApplicationService.login(PersonAssembler.toDO(personDTO));
+        } catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
